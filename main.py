b10bc3c711f2140ee112c5c2a61a04a88622cb78
Demid2010 <129614903+Demid2010@users.noreply.github.com>
2023-04-14 19:23:43 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import ranom
-password = "+-*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ21234567890"
-
-long = int(input("Какую длину пароля вы хотите?:"))
-save_password = "".join(random.saple(password,long))
-print(save_password)
